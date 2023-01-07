78e3b6b5eaede261f332eb25001a694654259d5a
tqa3001 <tqa3001@gmail.com>
2023-01-07 08:51:34 +0700

    fixed logger to mkdir when log folder is not found

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,13 +1,16 @@
 const fs = require('fs'); 
 const path = require('path'); 
 
-module.exports = async (req, res, next) => {
+module.exports = async (req, res, next) => {  // haha premature optimization async I/O why not (based)
+  const pathToFolder = path.join(__dirname, '..', 'logs'); 
   const data = Date() + '|' + req.method + '|' + req.url + '\n'; 
-  fs.appendFile(path.join(__dirname, '..', 'logs', 'requests.log'), data, (err) => {
-    if (err) {
-      console.log('Error adding to log', err); 
-    } else {
-      next(); 
-    }
-  });  
+  try {
+    await fs.promises.access(pathToFolder);
+  } catch(err) {
+    fs.promises.mkdir(pathToFolder); 
+  } finally {
+    fs.promises.appendFile(path.join(pathToFolder, 'requests.log'), data)
+      .then(next()) 
+      .catch((err) => { console.log('Error adding to log |', err); }); 
+  }
 }; 
