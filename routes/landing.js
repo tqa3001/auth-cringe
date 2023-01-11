49b23be7a4b83dc635f736026d46c0f08766b69c
tqa3001 <tqa3001@gmail.com>
2023-01-11 11:20:14 +0700

    refactored routes

--- routes/landing.js
+++ routes/landing.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'landing.html')); 
 }); 
 
+router.get('/login', (req, res) => { 
+  if (req.session.authenticated) {
+    res.redirect('/user'); 
+  } else {
+    res.sendFile(path.join(__dirname, '..', 'views', 'login.html')); 
+  }
+});
+
+router.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'register.html')); 
+}); 
+
 module.exports = router; 
