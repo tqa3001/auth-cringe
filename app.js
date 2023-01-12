415c7476088070c78bc52a72d4b2c5fd1f02c389
tqa3001 <tqa3001@gmail.com>
2023-01-12 11:32:24 +0700

    refactored routes

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 require('dotenv').config()
 const express = require('express'); 
-const cors = require('cors'); 
-const path = require('path'); 
 const mongoose = require('mongoose'); 
 const passport = require('passport'); 
-const LocalStrategy = require('passport-local').Strategy; 
+const cors = require('cors'); 
+const bodyParser = require('body-parser');
+const logger = require('./middlewares/logger.js'); 
 const session = require('express-session'); 
-// const store = new session.MemoryStore();  // for testing only, not for development.
-const MongoStore = require('connect-mongo');  // current issue: connecting to mongodb is hideously slow!
-const { getDB } = require('./services/database.js'); 
+const MongoStore = require('connect-mongo');  // current issue: connecting to mongodb is sorta slow!
+const LocalStrategy = require('passport-local').Strategy; 
 
 /* Mongoose */
 async function connectMongoDB() {
@@ -21,10 +20,10 @@ async function connectMongoDB() {
 }; 
 connectMongoDB(); 
 
-/* Express */
+/* Init */
 const app = express();
 app.use(cors());   // corsOptions not defined 
-app.use(require('./middlewares/logger.js')); 
+app.use(logger);  // or use morgan
 app.use(session({
   secret: "a key used for signing cookies", // development: make this long, private, and random. 
   resave: false,  
@@ -35,30 +34,34 @@ app.use(session({
     // touchAfter: 24 * 3600,  // update every 24 hours (default -> every time user refreshes?)
   })
 }));     
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json()); 
 
-// Print current state
+/* Configure passportjs */
+app.use(passport.initialize()); 
+app.use(passport.session());  // equiv to app.use(passport.authenticate('session'));
+
+/* Configure passport-local */
+const User = require('./schemas/user.js');  // Schema with passport.js plugin
+passport.use(new LocalStrategy(User.authenticate()));  // passport-local-mongoose implements a verify function for you 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+/* Print current state */
 app.use(async (req, res, next) => {
   console.log("Session id: ", req.sessionID); 
   console.log("Current session: ", req.session); 
-  console.log("Current DB: ");
-  console.log(await getDB()); 
   next(); 
 }); 
 
-/* Parsing POST body */
-app.use(require('body-parser').urlencoded({ extended: false })); 
-app.use(express.json());  // later versions of expressjs
-
 /* Routes */ 
 app.use('/', require('./routes/landing.js')); 
-app.use('/login', require('./routes/login.js')); 
-app.use('/signup', require('./routes/signup.js')); 
 app.use('/status', require('./routes/status.js')); 
 app.use('/user', require('./routes/user.js')); // not app.get('/user', require...) smh my ear
 app.use('/auth', require('./routes/auth.js')); 
  
 /* Add an error handler here */
 const PORT = process.env.PORT || 3000;  
-app.listen(PORT, () => {
+app.listen(PORT, () => { 
   console.log(`Listening on port ${PORT}`); 
 });
